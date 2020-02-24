553a5f0b670d6e720ce10430f5f30fe531a4db82
skyrebel <sylvain.santos@neuf.fr>
2020-02-24 10:22:34 +0000

    last modifs

--- childtheme/header.php
+++ childtheme/header.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html lang="fr-FR">
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     
-    <?php wp_head(); ?>
+    <!-- plein de choses entre -->
 </head>
 
 <body <?php body_class(); ?>>
-    
+  <header class="header">
+  <a href="http://wp.local/">
+      <img src="http://wp.local/wp-content/themes/gitbreakers/img/screenshot.png" alt="Logo" />
+</a>
+  </header>
     <?php wp_body_open(); ?>
