22123d0206e878d159d589f6d0a79f39a28460d0
Gragor58 <gragor46@gmail.com>
2020-02-25 11:00:12 +0000

    modif perso ne pas pull ce commit

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <body <?php body_class(); ?>>
   <header class="header-img">
     <a href="<?php echo home_url( '/' ); ?>">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/screenshot.png" alt="Logo">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/header.jpg" alt="Logo">
     </a>  
   </header>
     <?php wp_body_open(); ?>
