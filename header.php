fc654f0176d38a2eb18af4bbd292d423598557a8
skyrebel <sylvain.santos@neuf.fr>
2020-02-25 10:35:39 +0000

    last commit after modifs

--- header.php
+++ header.php
@@ -3,14 +3,20 @@
   <head>
       <meta charset="<?php bloginfo( 'charset' ); ?>" >
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-      
+
+<!-- **************** moteur de recherche ********************** -->
+
+      <?php get_search_form(); ?>
+
+      <!-- **************** end moteur de recherche***************** -->
+
       <?php wp_head(); ?>
   </head>
 
   <body <?php body_class(); ?>>
     <header class="header-img">
       <a href="<?php echo home_url( '/' ); ?>">
-        <img src="<?php echo get_template_directory_uri(); ?> ./assets/me" alt="Logo">
+        <img src="<?php echo get_template_directory_uri(); ?> ./" alt="Logo">
       </a>  
     </header>
       <?php wp_body_open(); ?>
