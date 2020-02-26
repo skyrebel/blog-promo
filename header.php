de5c6aacb66a83162e32351982898758ea00af9b
skyrebel <sylvain.santos@neuf.fr>
2020-02-26 08:29:51 +0000

    commit modifs php

--- header.php
+++ header.php
@@ -1,17 +1,9 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
-=======
-
->>>>>>> 46113bdd63bc12b393e58422cdac5bf4b965be6c
 <html <?php language_attributes(); ?> > <!-- langue du site -->
   <head>
       <meta charset="<?php bloginfo( 'charset' ); ?>" > <!-- encodage du site -->
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 46113bdd63bc12b393e58422cdac5bf4b965be6c
 <html lang="fr-FR">
 
 <head>
@@ -35,10 +27,6 @@
       <a href="<?php echo home_url( '/' ); ?>">
         <img src="<?php echo get_template_directory_uri(); ?> ./" alt="Logo">
       </a>  
-<<<<<<< HEAD
-=======
-
->>>>>>> 46113bdd63bc12b393e58422cdac5bf4b965be6c
       
       <?php wp_head(); ?> <!-- chargement scripts et styles, et titre activé ds function.php -->
   </head>
@@ -48,11 +36,8 @@
     <header>  <!-- logo du site -->
 
       <h1><img src="<?php echo get_template_directory_uri(); ?> ./assets/medias/logo.png" alt="Logo"></h1>
-<<<<<<< HEAD
-=======
       <h1><img src="<?php echo get_template_directory_uri(); ?> /assets/medias/logo.png" alt="Logo"></h1>
 
->>>>>>> 46113bdd63bc12b393e58422cdac5bf4b965be6c
     </header>
     
 
