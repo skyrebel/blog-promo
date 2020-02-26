9b5560c83a93ecaa4bb629bc591103b55993193a
fiona05 <fiona.p.gomes@gmail.com>
2020-02-26 09:24:33 +0000

    index

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
   <body> 
 
     <header>  <!-- logo du site -->
-      <h1><img src="<?php echo get_template_directory_uri(); ?> assets/medias/logo.png" alt="Logo"></h1>
+      <h1><img src="<?php echo get_template_directory_uri(); ?> ./assets/medias/logo.png" alt="Logo"></h1>
         
       <!-- menu -->
       <?php 
