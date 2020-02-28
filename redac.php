5e6d23cd9243acf5bd51303f9ae776288dbda5ac
fiona05 <fiona.p.gomes@gmail.com>
2020-02-28 09:45:22 +0000

    page redac

--- redac.php
+++ redac.php
@@ -8,45 +8,80 @@
 */
 ?>
 
-<?php get_header(); ?>
+  <?php get_header(); ?>
 
-<!--boucle: contrôle du contenu à afficher, affiche tt type de contenu, données de l'article à afficher ds la boucle ex the_title --> 
+    <main>
+      <!-- redac section -->
+      <div class="container-fluid mt-5">
+          <div class="row justify-content-center">
+              <div class="col-2 shadow mt-5 mx-4 py-4 bg-card-white text-center poppins">
+                <!-- redac identity card -->
+                <?php
+                // 1. on défini ce que l'on veut
+                $args = array(
+                    'post_type' => 'apprenants',
+                    'posts_per_page' => 12
+                );
+                // 2. on exécute la query
+                $apprenants_query = new WP_Query($args);
+                // 3. on lance la boucle !
+                if ($apprenants_query->have_posts()) : while ($apprenants_query->have_posts()) : $apprenants_query->the_post();
+                  echo '<p class="h5 pt-3 font-weight-bold">';
+                  echo get_post_meta($post->ID, '_apprenants_nom', true); 
+                  echo get_post_meta($post->ID, '_apprenants_prenom', true);
+                  echo '</p>';
+                  echo '<p class="pt-5 font-weight-bold">Liens utiles</p>';
+                  echo '<p class="mb-2 social-link">';
+                  echo get_post_meta($post->ID, '_apprenants_github', true);
+                  echo '</p>';
+                  echo '<p class="mb-2 social-link">';
+                  echo get_post_meta($post->ID, '_apprenants_linkedIn', true);
+                  echo '</p>';
+                  echo '<p class="social-link">';
+                  echo get_post_meta($post->ID, '_apprenants_portfolio', true);
+                  echo '</p>';  
+                endwhile;
+                endif;
+                // 4. On réinitialise à la requête principale (important)
+                wp_reset_postdata();
+                ?>
+              </div>
+          </div>
+          <div class="row justify-content-center mb-5">
+            <div class="col-2 shadow mb-5 mt-5 mx-4 py-4 bg-card-white text-center poppins">
+              <!-- redac identity card -->
+              <?php
+              // 1. on défini ce que l'on veut
+              $args = array(
+                  'post_type' => 'apprenants',
+                  'posts_per_page' => 4
+              );
+              // 2. on exécute la query
+              $apprenants_query = new WP_Query($args);
+              // 3. on lance la boucle !
+              if ($apprenants_query->have_posts()) : while ($apprenants_query->have_posts()) : $apprenants_query->the_post();
+                echo '<p class="h5 pt-3 font-weight-bold">';
+                echo get_post_meta($post->ID, '_apprenants_nom', true); 
+                echo get_post_meta($post->ID, '_apprenants_prenom', true);
+                echo '</p>';
+                echo '<p class="pt-5 font-weight-bold">Liens utiles</p>';
+                echo '<p class="mb-2 social-link">';
+                echo get_post_meta($post->ID, '_apprenants_github', true);
+                echo '</p>';
+                echo '<p class="mb-2 social-link">';
+                echo get_post_meta($post->ID, '_apprenants_linkedIn', true);
+                echo '</p>';
+                echo '<p class="social-link">';
+                echo get_post_meta($post->ID, '_apprenants_portfolio', true);
+                echo '</p>';  
+              endwhile;
+              endif;
+              // 4. On réinitialise à la requête principale (important)
+              wp_reset_postdata();
+              ?>
+          </div>
+          </div>
+      </div>
+    </main>
 
-<?php
-
-// 1. on défini ce que l'on veut
-$args = array(
-    'post_type' => 'apprenants',
-    'posts_per_page' => 2
-);
-
-// 2. on exécute la query
-$apprenants_query = new WP_Query($args);
-
-// 3. on lance la boucle !
- if ($apprenants_query->have_posts()) : while ($apprenants_query->have_posts()) : $apprenants_query->the_post();
-// Echo some markup
-// echo 'bouibouibouiboui';
-// As with regular posts, you can use all normal display functions, such as
-
-// Within the loop, you can access custom fields like so:
-echo '<p>';
-the_title();
-echo get_post_meta($post->ID, '_apprenants_nom', true) .'<br>'; 
-echo get_post_meta($post->ID, '_apprenants_prenom', true).'<br>';
-echo get_post_meta($post->ID, '_apprenants_github', true).'<br>';
-echo get_post_meta($post->ID, '_apprenants_linkedIn', true).'<br>';
-echo get_post_meta($post->ID, '_apprenants_portfolio', true).'<br>';
-echo '</p>';  
-// Or like so:
-// $personnes = get_post_custom_values('personnes');
-// echo $personnes[0];
-// echo 'yo'; // Markup closing tags.
-endwhile;
-endif;
-
-// 4. On réinitialise à la requête principale (important)
-wp_reset_postdata();
-?>
-
-<?php get_footer(); ?>
+  <?php get_footer(); ?>
