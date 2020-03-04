c3d3c7a3be9bb5abaf2c12e79ca64a66417a1a05
fiona05 <fiona.p.gomes@gmail.com>
2020-03-04 13:05:18 +0000

    tt sauf pagination

--- redac.php
+++ redac.php
@@ -12,44 +12,45 @@
 
     <main>
       <!-- redac section -->
-      <div class="container-fluid mt-5">
+      <div class="container-fluid my-5">
         <?php
           // 1. on défini ce que l'on veut
           $args = array(
               'post_type' => 'apprenants',
-              'posts_per_page' => 12,
+              'posts_per_page' => 16,
           );
           // 2. on exécute la query
           $apprenants_query = new WP_Query($args);
-          // 3. on lance la boucle !
-          if ($apprenants_query->have_posts()) : while ($apprenants_query->have_posts()) : $apprenants_query->the_post();
-            echo '<div class="row justify-content-center">';
-              echo '<div class="col-2 shadow mt-5 mx-4 py-4 bg-card-white text-center poppins">';
-                //redac identity card
-                echo '<p>';
-										echo the_post_thumbnail();
-								echo '</p>';               
-                echo '<p class="h5 pt-3 text-uppercase font-weight-bold">';
-                echo get_post_meta($post->ID, '_apprenants_nom', true); 
-                echo get_post_meta($post->ID, '_apprenants_prenom', true);
-                echo '</p>';
-                echo '<p class="pt-5 font-weight-bold">Liens utiles</p>';
-                echo '<p><a href="';
-                echo get_post_meta($post->ID, '_apprenants_github', true);
-                echo '" class="mb-2 social-link" target="_blank">Mon Github</a></p>';
-                echo '<p><a href="';
-                echo get_post_meta($post->ID, '_apprenants_linkedIn', true);
-                echo '" class="mb-2 social-link" target="_blank">Mon Linkedin</a></p>';
-                echo '<p><a href="';
-                echo get_post_meta($post->ID, '_apprenants_portfolio', true);
-                echo '" class="social-link" target="_blank">Mon Portfolio</a></p>';
+          echo '<div class="row justify-content-center">';
+            // 3. on lance la boucle !
+            if ($apprenants_query->have_posts()) : while ($apprenants_query->have_posts()) : $apprenants_query->the_post();
+                echo '<div class="col-2 shadow my-5 mx-5 py-4 bg-card-white text-center poppins">';
+                  //redac identity card
+                  echo '<p>';
+                      echo the_post_thumbnail('thumbnail', array('class' => 'img-fluid', 'alt' => 'avatar'));
+                  echo '</p>';               
+                  echo '<p class="h5 pt-3 text-uppercase font-weight-bold">';
+                  echo get_post_meta($post->ID, '_apprenants_nom', true); 
+                  echo '&nbsp;';
+                  echo get_post_meta($post->ID, '_apprenants_prenom', true);
+                  echo '</p>';
+                  echo '<p class="pt-5 font-weight-bold">Liens utiles</p>';
+                  echo '<p><a href="';
+                  echo get_post_meta($post->ID, '_apprenants_github', true);
+                  echo '" class="mb-2 social-link" target="_blank">Mon Github</a></p>';
+                  echo '<p><a href="';
+                  echo get_post_meta($post->ID, '_apprenants_linkedIn', true);
+                  echo '" class="mb-2 social-link" target="_blank">Mon Linkedin</a></p>';
+                  echo '<p><a href="';
+                  echo get_post_meta($post->ID, '_apprenants_portfolio', true);
+                  echo '" class="social-link" target="_blank">Mon Portfolio</a></p>';
                 echo '</div>';  
-            echo '</div>'; 
-          endwhile;
-          endif;
-          // 4. On réinitialise à la requête principale (important)
-          wp_reset_postdata();
-        ?>
+            endwhile;
+            endif;
+            // 4. On réinitialise à la requête principale (important)
+            wp_reset_postdata();
+          echo '</div>'; 
+        ?> 
       </div>
     </main>
 
--- single.php
+++ single.php
@@ -13,26 +13,39 @@
 	<!--boucle: contrôle du contenu à afficher, affiche tt type de contenu, données de l'article à afficher ds la boucle ex the_title --> 
 	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 		<!--  articles -->
-		<article class="post">
-			<h2><?php the_title(); ?></h2>   <!--titre article --> 
-
-			<?php if ( has_post_thumbnail() ): ?> <!--verif si img article --> 
-				<div class="post__thumbnail">
-					<?php the_post_thumbnail(); ?>
+		<div class="container-fluid bg-main-news">
+			<div class="container pt-5">
+				<div class="row text-center playfair py-4 my-4">
+					<div class="col-12">
+						<h2 class="font-weight-bold mb-5 h1"><?php the_title(); ?></h2>   <!--titre article --> 
+						<?php if ( has_post_thumbnail() ): ?> <!--verif si img article --> 
+						<div class="post__thumbnail">
+							<?php the_post_thumbnail('large', array('class' => 'img-fluid'));?>
+						</div>
+							<?php endif; ?>
+						<div class="post__meta mt-4">
+							<p>
+								Publié le <strong><?php the_date(); ?></strong><br> <!--date article --> 
+								par <strong><?php the_author(); ?></strong> <!--auteur article --> 
+							</p>
+						</div>
+					</div>
 				</div>
-			<?php endif; ?>
-
-			<div class="post__meta">
-				<p>
-				Publié le <?php the_date(); ?> <!--date article --> 
-				par <?php the_author(); ?> <!--auteur article --> 
-				</p>
 			</div>
-
-			<div class="post__content"> <!--contenu de l'article --> 
-				<?php the_content(); ?>
+		</div>
+		<div class="container-fluid">
+			<div class="container">
+				<div class="row mb-5 pb-5">
+					<div class="col-12">
+						<div class="post__content"> <!--contenu de l'article --> 
+							<?php the_content(); ?>
+						</div>
+					</div>
+				</div>
 			</div>
-		</article>		
+		</div>
+
 	<?php endwhile; endif; ?>
+	
 
 	<?php get_footer(); ?>
